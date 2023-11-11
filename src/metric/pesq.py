287ea9cc08df8141d8ad14ad153a88de8298f796
LeoProko <leoproko126@gmail.com>
2023-11-12 01:02:23 +0300

    wip

--- src/metric/pesq.py
+++ src/metric/pesq.py
@@ -15,11 +15,17 @@ class PESQMetric(BaseMetric):
         self.device = device
         self.pesq = PESQ(sr, "wb").to(device)
 
-    def __call__(self, pred, target, *args, **kwargs):
-        valid_size = min(pred.size(-1), target.size(-1))
+    def __call__(self, pred, target, length, *args, **kwargs):
+        lengths = [min(min(length[i], pred.size(-1)), target.size(-1)) for i in range(len(length))]
+
         pesqs = [
-            self.pesq(pred[i, 0, :valid_size].to(self.device), target[i, 0, :valid_size].to(self.device))
+            self.pesq(pred[i, 0, :lengths[i]].to(self.device), target[i, 0, :lengths[i]].to(self.device))
             for i in range(pred.size(0))
-            if not (target[i, 0, :valid_size] == 0).all() and not (pred[i, 0, :valid_size] == 0).all()
+            if not ( torch.isclose(target[i, 0, :lengths[i]].mean(), torch.tensor([0.0])) and (target[i, 0, :lengths[i]] > -1e-8).all()
+                 or
+                torch.isclose(pred[i, 0, :lengths[i]].mean(), torch.tensor([0.0])) and (pred[i, 0, :lengths[i]] > -1e-8).all()
+                or (target[i, 0, :lengths[i]] == 0).all()
+                or (pred[i, 0, :lengths[i]] == 0).all()
+            )
         ]
         return sum(pesqs) / len(pesqs) if pesqs else 0
